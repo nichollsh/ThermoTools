b4074d36afc411700a9e5c95e1329d2b2a7314fe
Harrison Nicholls <harrison.nicholls@physics.ox.ac.uk>
2024-06-28 16:43:36 +0100

    Updated plots

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 import matplotlib as mpl
 
 font = {'family' : 'Times',
-        'size'   : 11.5}
+        'size'   : 12}
 
 mpl.rc('font', **font)
 
@@ -9,3 +9,12 @@ import numpy as np
 import glob, os
 import matplotlib.pyplot as plt
 from scipy.interpolate import PchipInterpolator
+
+def pretty_name(form):
+    pretty = ""
+    for f in form:
+        if f.isnumeric():
+            f = r"$_{%d}$"%int(f)
+        pretty += f 
+    return pretty
+
