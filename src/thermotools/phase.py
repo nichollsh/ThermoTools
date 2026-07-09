241f57ae03067f84d2b0fbce8c422dd6633c6e9a
Harrison Nicholls <harrison.nicholls@ast.cam.ac.uk>
2026-07-09 12:40:20 +0100

    Add phase change data from Woitke+2017

--- src/thermotools/phase.py
+++ src/thermotools/phase.py
@@ -3,7 +3,11 @@ from scipy.interpolate import PchipInterpolator
 from scipy.integrate import quad
 import os
 
-from thermotools import get_gendir
+from thermotools import get_gendir, get_inpdir
+
+mmHg = 1/0.0075006156130264  # [Pa] from https://www.convertunits.com/from/mmHg/to/Pa
+dyne_per_cm2 = 1e-1  # [Pa] from https://www.convertunits.com/from/dyne/cm2/to/Pa
+bar = 1e5  # [Pa] from https://www.convertunits.com/from/bar/to/Pa
 
 # Antoine equation: fit for Psat as a function of temperature
 # Most of these values taken from NIST Webbook
@@ -209,3 +213,147 @@ def cc_psat(t:float, gas:str, mmw:float):
     out = np.exp(rhs) * p_ref 
     return out 
 
+def woitke_load_gas(gas:str):
+    # load gas data from Woitke+2017 derived CSV File
+    # these are from Table D2
+    fname = os.path.join(get_inpdir(),"psat","web", "woitke2017_TableD2.csv")
+    data = np.loadtxt(fname, delimiter=',', dtype=str, comments='#', skiprows=1).T
+
+    # locate row for this gas
+    igas = -1
+    for i,g in enumerate(data[1]):
+        if gas in g:
+            print("Querying Woitke+2017 phase change data for %s"%gas)
+            if igas>-1:
+                print(f"    multiple entries for {gas}, found: {data[1][igas]} and {data[1][i]}")
+                if "[s" in data[2][i]:
+                    igas = i
+            else:
+                igas = i
+
+    # not found?
+    if igas == -1:
+        print("Gas %s not found in Woitke+2017 phase change data (Table D2)"%gas)
+        return None
+    else:
+        print(f"    using {data[1][igas]} data for {gas}")
+    
+    def _val_or_default(val, default):
+        if val == "":
+            return default
+        else:
+            return float(val)
+
+    # convert to dictionary
+    data = {
+        # metadata
+        "gas": str(data[1][igas]).split("[")[0],
+        "fit_type": int(data[2][igas]),
+        "T_min": _val_or_default(data[4][igas], 0.0),
+        "T_max": _val_or_default(data[5][igas], 1e9),
+
+        # density of condensed phase (g/cm^3)
+        "density": float(data[3][igas]),
+
+        # fit coefficients
+        "c0": _val_or_default(data[6][igas],  0.0),
+        "c1": _val_or_default(data[7][igas],  0.0),
+        "c2": _val_or_default(data[8][igas],  0.0),
+        "c3": _val_or_default(data[9][igas],  0.0),
+        "c4": _val_or_default(data[10][igas], 0.0),
+    }
+
+    # only store cases with fits for psat, skip deltaG cases
+    if data["fit_type"] in [1,2,5]:
+        return None
+
+    return data
+
+def woitke_load_all():
+    # load all Woitke+2017 data
+    fname = os.path.join(get_inpdir(),"psat","web", "woitke2017_TableD2.csv")
+    data = np.loadtxt(fname, delimiter=',', dtype=str, comments='#', skiprows=1).T
+
+    parsed_data = {}
+    seen_gases = list(gases.keys()) # don't load these twice
+
+    for i in range(len(data[1])):
+        gas = str(data[1][i]).split("[")[0]
+        if gas not in seen_gases:
+            gas_data = woitke_load_gas(gas)
+            if gas_data is not None:
+                parsed_data[gas] = gas_data
+                seen_gases.append(gas)
+
+    return parsed_data
+
+# load the data globally
+woitke_data = woitke_load_all()
+woitke_gases = list(woitke_data.keys())
+print("Loaded Woitke+2017 phase change data for %d gases"%len(woitke_gases))
+print("    gases: %s"%(", ".join(woitke_gases)))
+
+def woitke_psat(t:float, gas:str):
+    # calculate saturation pressure using Woitke+2017 data
+
+    # get this gas
+    if gas not in woitke_gases:
+        raise Exception("Gas %s not found in Woitke+2017 phase change data (Table D2)"%gas)
+    gas_data = woitke_data[gas]
+    c0 = gas_data["c0"]
+    c1 = gas_data["c1"]
+    c2 = gas_data["c2"]
+    c3 = gas_data["c3"]
+    c4 = gas_data["c4"]
+
+    # handle temperature range
+    t = float(t)
+    t = max(t, gas_data["T_min"])
+    t = min(t, gas_data["T_max"])
+
+    # celcius for some fits
+    tc = t - 273.15
+
+    # these are the various fit equations
+    match gas_data["fit_type"]:
+        case 1:
+            #  Sharp & Huebner (1990)
+            raise Exception("Woitke+2017 fit type 1 not yet implemented")
+        case 2:
+            #  NIST-JANAF data (Chase 1986) 
+            raise Exception("Woitke+2017 fit type 2 not yet implemented")
+        case 3:
+            #  NIST-JANAF data (Chase 1986) 
+            ln_P =  c0 / t + c1 + c2 * t + c3 * t**2 + c4 * t**3
+            return np.exp(ln_P) * dyne_per_cm2 # dyne/cm^2 to Pa
+        case 4:
+            #  NIST-JANAF data (Chase 1986) 
+            ln_P = c0 + c1 / (t + c2)
+            return np.exp(ln_P) * dyne_per_cm2 # dyne/cm^2 to Pa
+        case 5:
+            #  NIST-JANAF data (Chase 1986) 
+            raise Exception("Woitke+2017 fit type 5 not yet implemented")
+        case 6:
+            #  Yaws (1999)
+            log10_P = c0 + c1 / t + c2 * np.log10(t) + c3 * t + c4 * t**2
+            return 10**log10_P * mmHg # mmHg to Pa
+        case 7:
+            #  Ackerman & Marley (2001)
+            P = c0 * np.exp( (c1*tc + tc**2/c2) / (tc + c3) )
+            return P * dyne_per_cm2 # dyne/cm^2 to Pa
+        case 8:
+            #  Weast (1971)
+            ln_P = c0 + c1 / t + c2 / t**2
+            return np.exp(ln_P) * bar # bar to Pa
+        case 9:
+            #   Prydz & Goodwin (1972)
+            log10_P = c0 + c1/(t + c2)
+            return 10**log10_P * bar # bar to Pa
+        case 10:
+            # Gail et al. (2013)
+            ln_P = c0/t + c1
+            return np.exp(ln_P) * dyne_per_cm2 # dyne/cm^2 to Pa
+        
+        case _:
+            raise Exception("Woitke+2017 fit type %d not yet implemented"%gas_data["fit_type"])
+        
